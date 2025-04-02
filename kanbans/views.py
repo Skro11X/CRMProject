b5112540a6f39405dca3d250daceb1229cf0fb54
Skro11X <via_atm@mail.ru>
2025-04-02 21:08:04 +0300

    fix double query

--- kanbans/views.py
+++ kanbans/views.py
@@ -11,10 +11,8 @@ class KanbanDetail(DetailView):
 
     def get_object(self, queryset=None):
         pk = self.kwargs.get(self.pk_url_kwarg)
-        list_of_fields = dict(KanbanField.objects.filter(kanban=pk).values_list("id", "position"))
         list_of_leads = Lead.objects.select_related('kanban_field__kanban').filter(kanban_field__kanban=pk)
-        kanban_columns = {values: [] for values in list_of_fields.values()}
+        kanban_columns = {}
         for lead in list_of_leads:
-            if lead.kanban_field.id in list_of_fields.keys():
-                kanban_columns[lead.kanban_field.position].append(lead)
+            kanban_columns.setdefault(lead.kanban_field.position).append(lead)
         return kanban_columns
